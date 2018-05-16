8a17d59f1335f249de38a0c547f18fbdf23fd955
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-05-16 14:21:26 +0900

    user input

--- routes/api/auth/auth.controller.js
+++ routes/api/auth/auth.controller.js
@@ -3,46 +3,45 @@ const crypto = require('crypto');
 const mysql = require('mysql');
 const config = require('../../../config');
 const conn = mysql.createConnection(config);
+const lineReader = require('line-reader');
 
 exports.register = (req, res) => {
 	const secret = req.app.get('jwt-secret');
-	const { username, password, school } = req.body;
-	const encrypted = crypto.createHmac('sha1', config.secret)
-		.update(password)
-		.digest('base64');
-	conn.query('SELECT * from users WHERE username=?', [username], (err, rows) => {
-		if (err) throw err;
-		if (rows.length == 0) {
-			conn.query(
-				'INSERT INTO users(username, password, school) VALUES (?, ?, ?)',
-				[username, encrypted, school],
-				(err, result) => {
-					if (err) throw err;
-					console.log(result);
-					jwt.sign(
-						{
-							_id: result.insertId,
-							username: username
-						},
-						secret,
-						{
-							expiresIn: '7d',
-							issuer: 'ideathon',
-							subject: 'userInfo'
-						}, (err, token) => {
-							if (err) return res.status(406).json({ message: 'register failed' });
-							return res.status(200).json({
-								message: 'registered successfully',
-								token
-							});
-						});
-				});
-		} else {
-			return res.status(406).json({
-				message: 'username exists'
-			})
-		}
-	});
+	// lineReader.eachLine('users.txt', function(line, last) {
+	// 	console.log(line);
+	// 	let row = line.split(' ');
+	// 	// let encrypted = 
+	// 	conn.query(
+	// 		'insert into users(username, password, team_id, school, admin) values(?, ?, ?, ?, ?)',
+	// 		[row[1], crypto.createHmac('sha1', config.secret).update(row[2]).digest('base64'), 0, row[0], 1],
+	// 		(err, result) => {
+	// 			if (err) throw err;
+	// 		}
+	// 	)
+	// 	if(last){
+	// 		// or check if it's the last one
+	// 		return res.status(200).json({
+	// 			message: 'input users done'
+	// 		})
+	// 	}
+	// });lineReader.eachLine('users.txt', function(line, last) {
+	// 	console.log(line);
+	// 	let row = line.split(' ');
+	// 	// let encrypted = 
+	// 	conn.query(
+	// 		'insert into users(username, password, team_id, school, admin) values(?, ?, ?, ?, ?)',
+	// 		[row[1], crypto.createHmac('sha1', config.secret).update(row[2]).digest('base64'), 0, row[0], 1],
+	// 		(err, result) => {
+	// 			if (err) throw err;
+	// 		}
+	// 	)
+	// 	if(last){
+	// 		// or check if it's the last one
+	// 		return res.status(200).json({
+	// 			message: 'input users done'
+	// 		})
+	// 	}
+	// });
 };
 
 exports.login = (req, res) => {
